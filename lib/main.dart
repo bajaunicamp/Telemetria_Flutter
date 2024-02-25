165fbd76a6898eabe73b52fd0253092f6e486384
Giancarlo Bonvenuto <giancarlo.bonvenuto@gmail.com>
2024-02-25 13:43:27 -0300

    Servidor e gráficos funcionando

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
+
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:telemetria_baja/pages/mapa.dart';
 import 'package:telemetria_baja/pages/telemetria.dart';
@@ -9,8 +13,14 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
 
   @override
   Widget build(BuildContext context) {
@@ -24,36 +34,190 @@ class MainApp extends StatelessWidget {
           title: const Text("Telemetria Baja"),
           backgroundColor: const Color.fromARGB(255, 48, 158, 53),
         ),
-        body: Center(
-          child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Builder(
-              builder: (context) => TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Color.fromARGB(255, 35, 187, 22),
-                  textStyle: const TextStyle(fontSize: 80)
-                ),
-                onPressed: (){},
-                child: const Text("Mapa")
-              )
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Builder(builder: (context) => ElevatedButton(
+                    onPressed: (){
+                      Navigator.pushNamed(context, '/mapa');
+                    },
+                    child: Text("Mapa")
+                    )
+                  ),
+                  Builder(builder: (context) => ElevatedButton(
+                    onPressed: (){
+                      Navigator.pushNamed(context, '/telemetria');
+                    },
+                    child: Text("Telemetria") 
+                    )
+                  )
+                ],
+              ) 
             ),
-            Builder(
-              builder: (context) => TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Color.fromARGB(255, 35, 187, 22),
-                  textStyle: const TextStyle(fontSize: 80)
-                ),
-                onPressed: (){
-                  Navigator.pushNamed(context, '/telemetria');
-                },
-                child: const Text("Telemetria")
-              )
-            ),
+            widgetServidor()
           ],
+        ),
+      )
+    );
+  }
+
+  String serverIP = "0.0.0.0";
+  TextEditingController serverIP_controller = TextEditingController();
+  int port = 0;
+  TextEditingController port_controller = TextEditingController();
+  bool conectado = false;
+  late Socket socket;
+
+  // ignore: non_constant_identifier_names
+  Widget widgetServidor() {
+    // ignore: sized_box_for_whitespace
+    Widget conectar = Container(
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      height: 150,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Server IP",
+                    ),
+                    controller: serverIP_controller,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Porta",
+                    ),
+                    controller: port_controller,
+                  ),
+              ]
+            )
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            child: ElevatedButton(
+              onPressed: (){
+                setState(() {
+                    if (serverIP_controller.text != ""){
+                      serverIP = serverIP_controller.text;
+                    }
+                    if (port_controller.text != "") {
+                      port = int.parse(port_controller.text);
+                    }
+                });
+                conectarAoServidor();
+                setState(() {
+                  conectado = true;
+                });
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 11, 230, 40),
+                elevation: 20
+              ),
+              child: Text("Conectar"),
+            )
+          )
+        ]
+      )
+    );
+    
+    Widget desconectar = Container(
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      height: 150,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Server IP: $serverIP"),
+              Text("Porta: $port")
+            ]
           )
         ),
-      ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            child: ElevatedButton(
+              onPressed: (){
+                setState(() {
+                    conectado = false;
+                    print("[END] Desconectado do Servidor ${socket.remoteAddress.address}:${socket.remotePort}");
+                    enviarMensagemAoServidor("!DISCONNECT");
+                    socket.destroy();
+                });
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 230, 11, 11),
+                elevation: 20
+              ),
+              child: Text("Desonectar"),
+            )
+          )
+        ]
+      )
     );
+    
+    if (!conectado){
+      return conectar;
+    }
+    else{
+      return desconectar;
+    }
+  }
+  
+  Future<void> conectarAoServidor() async{
+    socket = await Socket.connect(serverIP, port);
+    print("[CONECTADO] Cliente conectado ao servidor ${socket.remoteAddress.address}:${socket.remotePort}");
+    
+    socket.listen(
+      (Uint8List data) {
+        final serverResponse = String.fromCharCodes(data);
+        
+        List<String> splitResponse = serverResponse.split(" ");
+        // Se for uma resposta de GPS
+        if (splitResponse[0].compareTo("[GPS]") == 0){
+          print("Dados do GPS recebidos");
+        }
+        // Se for uma resposta do Combustível
+        else if (splitResponse[0].compareTo("[COMBUSTIVEL]") == 0){
+          print("Dados do combustível recebidos");
+          setState(() {
+              Dados.inserirDado(Dados.combustivel, double.parse(splitResponse[1]));
+          });
+        } 
+        // Se não for nenhuma das respostas esperadas, apenas imprimí-la
+        else {
+          print("[SERVER] $serverResponse");
+        }
+      },
+      onError: (error){
+        print("[ERROR] $error");
+        socket.destroy();
+      },
+      onDone: (){
+        print("[END] A conexão com o servidor terminou");
+        socket.destroy();
+      }
+
+      );
+  } 
+  
+  void enviarMensagemAoServidor(String mensagem) {
+    int messageLength = mensagem.length;
+    socket.write(messageLength.toString());
+    socket.write(mensagem);
   }
 }
--- lib/pages/telemetria.dart
+++ lib/pages/telemetria.dart
@@ -1,34 +1,41 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
-import 'package:flutter/widgets.dart';
 
-class Telemetria extends StatelessWidget {
-  const Telemetria({super.key});
-  
-  // Função para criar diversos gráficos com o mesmo design
-  LineChart grafico(double minX, double maxX, double minY, double maxY, List<FlSpot> spots) {
-    return LineChart(
-          LineChartData(
-            minX: minX, maxX: maxX, minY: minY, maxY: maxY,
-            gridData: FlGridData(
-              show: true,
-              getDrawingHorizontalLine: (value) =>
-              FlLine(
-                color: Color.fromARGB(139, 255, 0, 0),
-                strokeWidth: 2,
-              )
-            ),
-            lineBarsData: [
-              LineChartBarData(
-                spots: spots,
-              )
-            ]
+// Função para criar diversos gráficos com o mesmo design
+LineChart criarGrafico(String title, double minX, double maxX, double minY, double maxY, List<FlSpot> spots) {
+return LineChart(
+      LineChartData(
+        titlesData: FlTitlesData(topTitles: AxisTitles(
+          axisNameWidget: Text(title)
+        )),
+        minX: minX, maxX: maxX, minY: minY, maxY: maxY,
+        gridData: FlGridData(
+          show: true,
+          getDrawingHorizontalLine: (value) =>
+          FlLine(
+            color: Color.fromARGB(139, 255, 0, 0),
+            strokeWidth: 2,
           )
-    );
-  }
+        ),
+        lineBarsData: [
+          LineChartBarData(
+            spots: spots,
+          )
+        ]
+      )
+);
+}
+
+class Telemetria extends StatefulWidget {
+  const Telemetria({super.key});
+
+  @override
+  State<Telemetria> createState() => _TelemetriaState();
+}
+
+class _TelemetriaState extends State<Telemetria> {
 
   @override
   Widget build(BuildContext context) {
@@ -38,47 +45,71 @@ class Telemetria extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          Container(
+          SizedBox(
             height: 500,
-              child: grafico(
-                0, 10, 0, 10,
-                [
-                  FlSpot(0, 0),
-                  FlSpot(1, 1),
-                  FlSpot(2, 3),
-                  FlSpot(4, 5),
-                  FlSpot(5, 8),
-                ]
-              )
-            ),
-          Container(
-            height: 500,
-              child: grafico(
-                0, 10, 0, 10,
-                [
-                  FlSpot(0, 0),
-                  FlSpot(1, 8),
-                  FlSpot(2, 9),
-                  FlSpot(4, 4),
-                  FlSpot(5, 3),
-                ]
-              )
-            ),
-          Container(
-            height: 500,
-              child: grafico(
-                0, 10, 0, 10,
-                [
-                  FlSpot(0, 0),
-                  FlSpot(3, 5),
-                  FlSpot(8, 3),
-                  FlSpot(9, 2),
-                  FlSpot(10, 2),
-                ]
-              )
-            ),
+            child: Dados.combustivel
+          ),
         ],
       )
     );
   }
 }
+
+// Esta classe serve para disponibilizar os dados para público
+class Dados{
+  
+  static void inserirDado(LineChart lineChart, y) {
+    int dataLength = lineChart.data.lineBarsData[0].spots.length;
+    // Enquanto o gráfico não tiver sido preenchido até a borda direita,
+    // Apenas adicionar dados sem mover o gráfico
+    if( dataLength < lineChart.data.maxX){
+      lineChart.data.lineBarsData[0].spots.add(
+        FlSpot(dataLength.toDouble(), y)
+      );
+    }
+    // Se a quantidade de dados fizer com que seja necessário o scroll
+    else{
+      print("Iniciando scroll");
+      // Primeiramente diminuímos em 1 o x dos spots seguintes
+      for(int i = 1; i<dataLength; i++){
+        double current_y = lineChart.data.lineBarsData[0].spots[i].y;
+        print("[$i, $current_y] -> [${i-1}, $current_y]");
+        lineChart.data.lineBarsData[0].spots[i-1] = FlSpot(i-1, current_y);
+      }
+      
+      // Em seguida, adicionamos o novo spot na última posição
+      lineChart.data.lineBarsData[0].spots[dataLength-1] = FlSpot(dataLength-1, y);
+      print("[20, ${lineChart.data.lineBarsData[0].spots[dataLength-1].y}]");
+    }
+  }
+
+  static LineChart combustivel = criarGrafico(
+    "combustivel (%)",
+    0, 20, 0, 100,
+    [
+      FlSpot(0, 0),
+      FlSpot(1, 10),
+      FlSpot(2, 15),
+      FlSpot(3, 15.6),
+      FlSpot(4, 20),
+      FlSpot(5, 21),
+      FlSpot(6, 25),
+      FlSpot(7, 25.6),
+      FlSpot(8, 24),
+      FlSpot(9, 20),
+      FlSpot(10, 20.1),
+      FlSpot(11, 50.2),
+      FlSpot(12, 55),
+      FlSpot(13, 14.2),
+      FlSpot(14, 51),
+      FlSpot(15, 51),
+      FlSpot(16, 12),
+      FlSpot(17, 92),
+      FlSpot(18, 32),
+      FlSpot(19, 64),
+      FlSpot(20, 14),
+    ]
+  );
+}
+
+
